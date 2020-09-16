ffedbe2b2fe47b241ce79749e7d97cad422708db
Tibor Ádám Szőke <szoke.tibi97@gmail.com>
2020-09-16 04:27:14 +0200

    Added sorting function

--- Forma1Teams/Pages/Teams/Index.cshtml.cs
+++ Forma1Teams/Pages/Teams/Index.cshtml.cs
@@ -19,11 +19,43 @@ namespace Forma1Teams.Pages.Teams
             _context = context;
         }
 
+        public string NameSort { get; set; }
+        public string FoundationSort { get; set; }
+        public string WinSort { get; set; }
         public IList<Team> Teams { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Teams = await _context.Teams.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            FoundationSort = sortOrder == "foundation_asc" ? "foundation_desc" : "foundation_asc";
+            WinSort = sortOrder == "win_asc" ? "win_desc" : "win_asc";
+
+            IQueryable<Team> teamsIQ = from t in _context.Teams
+                                       select t;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    teamsIQ = teamsIQ.OrderByDescending(t => t.Name);
+                    break;
+                case "foundation_asc":
+                    teamsIQ = teamsIQ.OrderBy(t => t.YearOfFoundation);
+                    break;
+                case "foundation_desc":
+                    teamsIQ = teamsIQ.OrderByDescending(t => t.YearOfFoundation);
+                    break;
+                case "win_asc":
+                    teamsIQ = teamsIQ.OrderBy(t => t.WorldChampionNumber);
+                    break;
+                case "win_desc":
+                    teamsIQ = teamsIQ.OrderByDescending(t => t.WorldChampionNumber);
+                    break;
+                default:
+                    teamsIQ = teamsIQ.OrderBy(t => t.Name);
+                    break;
+            }
+
+            Teams = await teamsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
